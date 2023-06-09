1983e5e1e11418a6b4e082cb13b694e1dbee862a
Hilbert-Laptop <cyaa@umich.edu>
2023-06-10 02:10:30 +0800

    feat: impl. minimal RegEx support in find

--- user/find.c
+++ user/find.c
@@ -4,6 +4,13 @@
 #include "kernel/fs.h"
 #define PATHBUFSIZ 512
 
+// Regexp matcher from Kernighan & Pike,
+// The Practice of Programming, Chapter 9.
+
+int matchhere(char*, char*);
+int matchstar(int, char*, char*);
+int match(char*, char*);
+
 char *fmtname(char *path) {
   char *p;
 
@@ -13,7 +20,7 @@ char *fmtname(char *path) {
   return p + 1;
 }
 
-void find(char *path, char *filename) {
+void find(char *path, char *pattern) {
   char path_buf[PATHBUFSIZ], *p;
   int fd;
   struct dirent de;
@@ -33,7 +40,7 @@ void find(char *path, char *filename) {
   switch (st.type) {
   case T_FILE: {
     // note: inode type = 2
-    if (strcmp(fmtname(path), filename) == 0) {
+    if (match(pattern, fmtname(path))) {
       fprintf(stdout, "%s\n", path);
     }
     break;
@@ -54,7 +61,7 @@ void find(char *path, char *filename) {
         continue;
       memmove(p, de.name, DIRSIZ);
       p[DIRSIZ] = '\0';
-      find(path_buf, filename);
+      find(path_buf, pattern);
     }
     break;
   }
@@ -74,4 +81,40 @@ int main(int argc, char *argv[]) {
   }
   find(argv[1], argv[2]);
   exit(0);
+}
+
+int
+match(char *re, char *text)
+{
+  if(re[0] == '^')
+    return matchhere(re+1, text);
+  do{  // must look at empty string
+    if(matchhere(re, text))
+      return 1;
+  }while(*text++ != '\0');
+  return 0;
+}
+
+// matchhere: search for re at beginning of text
+int matchhere(char *re, char *text)
+{
+  if(re[0] == '\0')
+    return 1;
+  if(re[1] == '*')
+    return matchstar(re[0], re+2, text);
+  if(re[0] == '$' && re[1] == '\0')
+    return *text == '\0';
+  if(*text!='\0' && (re[0]=='.' || re[0]==*text))
+    return matchhere(re+1, text+1);
+  return 0;
+}
+
+// matchstar: search for c*re at beginning of text
+int matchstar(int c, char *re, char *text)
+{
+  do{  // a * matches zero or more instances
+    if(matchhere(re, text))
+      return 1;
+  }while(*text!='\0' && (*text++==c || c=='.'));
+  return 0;
 }
