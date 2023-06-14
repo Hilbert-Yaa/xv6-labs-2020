3b7c13efbdc8f122467caffae5461b72a41e1c96
Hilbert-Laptop <cyaa@umich.edu>
2023-06-14 19:08:14 +0800

    chore: format sys_trace function

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -6,6 +6,7 @@
 #include "memlayout.h"
 #include "spinlock.h"
 #include "proc.h"
+#include "sysinfo.h"
 
 uint64 sys_exit(void) {
   int n;
@@ -79,6 +80,5 @@ uint64 sys_uptime(void) {
 uint64 sys_trace(void) {
   if (argint(0, &(myproc()->tracemask)) < 0)
     return -1;
-  ;
   return 0;
 }
